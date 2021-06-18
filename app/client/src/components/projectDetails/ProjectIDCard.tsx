f345ebaeba2e913e506835355a4dd7894e2f0a53
SamFlueck <71517130+suzalflueck@users.noreply.github.com>
2021-06-18 16:16:58 -0700

    moved changes to new branch to keep git workflow granular.

---
+++ app/client/src/components/projectDetails/ProjectIDCard.tsx
@@ -0,0 +1,60 @@
+//
+// Copyright © 2020 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import React from 'react';
+import { Box, Flex, Text } from 'rebass';
+import theme from '../../theme';
+import CardItem from './CardItem';
+
+interface IProjectIDCardProps {
+  name?: string;
+  description?: string;
+  ministry?: string;
+  program?: string;
+  CPS?: string;
+  ministryProjectNumber?: string;
+}
+
+const ProjectIDCard: React.FC<IProjectIDCardProps> = (props) => {
+  const {
+    name = '',
+    description = '',
+    ministry = '',
+    program = '',
+    CPS = '',
+    ministryProjectNumber = ''
+  } = props;
+  
+  return (
+    <Box>
+      <Flex alignItems="center" justifyContent="center" flexDirection="column">
+        <Box alignItems="left" width={3 / 4}>
+          <Text as="h2">
+          Project Identification
+          </Text>
+        </Box>
+        <CardItem label={'Project Name'} content={name} />
+        <CardItem label={'Project Description'} content={description} />
+        <CardItem label={'Ministry'} content={ministry} />
+        <CardItem label={'Program Name'} content={program} />
+        <CardItem label={'CPS Identifier'} content={CPS} />
+        <CardItem label={'Ministry Project Number'} content={ministryProjectNumber} />
+      </Flex>
+    </Box>
+  );
+};
+
+export default ProjectIDCard;
