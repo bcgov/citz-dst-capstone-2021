0a7fe8bc076bd3b85307f6ecdfdba52599492189
SamFlueck <71517130+suzalflueck@users.noreply.github.com>
2021-07-19 15:02:09 -0700

    Added menu that toggles in/out from left
    Fix line on header
    
    menu icon expands user menu from left

--- app/client/src/components/Header.tsx
+++ app/client/src/components/Header.tsx
@@ -18,14 +18,18 @@ import { useHistory, Link } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { connect } from 'react-redux';
 
-import { Box, Flex, Text } from 'rebass';
+import { Box, Drawer, IconButton } from '@material-ui/core';
+
+import MenuIcon from '@material-ui/icons/Menu';
+
 import React from 'react';
-import theme from '../theme';
 import typography from '../typography';
 import GovLogo from './common/GovLogo';
 import Button from './common/buttons/Button';
+import MenuOption from './common/buttons/MenuOption';
 import { StoreState, User } from '../types';
 import { logout } from '../actions';
+import theme from '../theme';
 
 const StyledHeader = styled.header`
   background-color: ${theme.colors.primary};
@@ -33,7 +37,7 @@ const StyledHeader = styled.header`
   position: fixed;
   top: 0;
   width: 100%;
-  z-index: ${theme.zIndices[1]};
+  z-index: ${theme.zIndices[5]};
 `;
 
 const StyledBanner = styled.div`
@@ -42,16 +46,20 @@ const StyledBanner = styled.div`
   display: flex;
   flex-direction: row;
   height: ${theme.navBar.desktopFixedHeight};
-  padding-left: ${theme.spacingIncrements[0]};
+  width: 100%;
   padding-right: ${theme.spacingIncrements[0]};
   border-bottom: 2px solid ${theme.colors.bcorange};
 `;
 
-const StyledText = styled(Text)`
+const StyledTypography = styled.h2`
   ${typography.toString()}
+  color: ${theme.colors.contrast};
   text-decoration: none;
-  font-weight: bold;
+  font-weight: 500;
+  font-size: ${[3, 4, 4]};
   min-width: 150px;
+  padding-left: ${[3, 0, 0]};
+  display: contents;
 `;
 
 interface HeaderProps {
@@ -62,6 +70,16 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = (props) => {
   const history = useHistory();
   const { email } = props.user;
+  const [menuOpen, setMenuOpen] = React.useState(false);
+
+  const headerMenuOptions = [
+    {label: 'Notifications', icon:'NotificationsIcon'},
+    {label: 'Account', icon:'AccountCircleIcon'},
+    {label: 'Settings', icon:'SettingsIcon'},
+    {label: 'Email', icon:'EmailIcon'},
+    {label: 'Calendar', icon:'CalendarTodayIcon'},
+    {label: 'About', icon:'InfoIcon'},
+  ];
 
   const redirectLogin = () => {
     if (email) {
@@ -72,34 +90,65 @@ const Header: React.FC<HeaderProps> = (props) => {
     }
   };
 
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleMenuItem = () => {
+    alert('TODO: Implement Menu Item');
+  };
+
   return (
-    <StyledHeader>
-      <Flex alignItems="center" sx={{ paddingRight: 3 }}>
-        <StyledBanner>
-          <Link
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              textDecoration: 'none',
-            }}
-            to="/"
-          >
-            <GovLogo />
-            <StyledText
-              as="h2"
-              color={theme.colors.contrast}
-              fontSize={[3, 4, 4]}
-              fontWeight={500}
-              pl={[3, 0, 0]}
+    <>
+      <StyledHeader>
+        <Box display="flex" alignItems="center" justifyContent="space-between">
+          <StyledBanner>
+            {email ? 
+              <IconButton
+                onClick={handleMenuToggle}
+              >
+                <MenuIcon fontSize="large" style={{'color': (menuOpen ? theme.colors.bcorange : theme.colors.contrast)}}/>
+              </IconButton>
+            : 
+            <></>
+            }
+            <Link
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                textDecoration: 'none',
+              }}
+              to="/"
             >
-              Reporting and Dashboard Service Improvement
-            </StyledText>
-          </Link>
-        </StyledBanner>
-        <Box mx="auto" />
-        <Button onClick={redirectLogin}>{email ? 'Logout' : 'Login'}</Button>
-      </Flex>
-    </StyledHeader>
+              <Box pl={email ? 0 : theme.spacingIncrements[0]}>
+                <GovLogo />
+              </Box>
+              <StyledTypography>
+                Reporting and Dashboard Service Improvement
+              </StyledTypography>
+            </Link>
+            <Box mx="auto" />
+            <Button onClick={redirectLogin}>{email ? 'Logout' : 'Login'}</Button>
+          </StyledBanner>
+        </Box>
+      </StyledHeader>
+      {email ? 
+      <Box>
+        <Drawer
+          variant="persistent"
+          anchor="left"
+          open={menuOpen}
+        >
+          <Box height={theme.navBar.desktopFixedHeight} />
+          {headerMenuOptions.map((option) => (
+            <MenuOption event={handleMenuItem} text={option.label} icon={option.icon} />
+          ))}
+        </Drawer>
+      </Box>
+      :
+      <></>
+      }
+    </>
   );
 };
 
