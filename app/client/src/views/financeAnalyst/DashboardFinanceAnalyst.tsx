d7d8c978afd1c74ab4f55e464c610ba692a3ea45
SamFlueck <71517130+suzalflueck@users.noreply.github.com>
2021-07-21 21:21:12 -0700

    Basic function over form dashboard for finance analyst

--- app/client/src/views/financeAnalyst/DashboardFinanceAnalyst.tsx
+++ app/client/src/views/financeAnalyst/DashboardFinanceAnalyst.tsx
@@ -14,18 +14,84 @@
 // limitations under the License.
 //
 
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { Typography, Box, Grid, Button } from "@material-ui/core";
 import { connect } from 'react-redux';
+import { ReportState, Report } from '../../types';
+import useApi from '../../utils/api';
 
 const DashboardFinanceAnalyst: React.FC = () => {
+  const history = useHistory();
+  const api = useApi();
+  // TODO: better way to only grab required data from api instead of entire reports
+  const [reportsInProgress, setReportsInProgress] = React.useState(0);
+  const [reportsSubmitted, setReportsSubmitted] = React.useState(0);
+  const [reportsToFollowup, setReportsToFollowup] = React.useState(0);
+  const [reportsApproved, setReportsApproved] = React.useState(0);
+
+  useEffect(()=> {
+    api.getReports('',{ state: ReportState.Draft }).then(data => {
+      setReportsInProgress(data.length);
+    });
+    api.getReports('',{ state: ReportState.Submitted }).then(data => {
+      setReportsSubmitted(data.length);
+    });
+    api.getReports('',{ state: ReportState.FollowUpRequired }).then(data => {
+      setReportsToFollowup(data.length);
+    });
+    api.getReports('',{ state: ReportState.Approved }).then(data => {
+      setReportsApproved(data.length);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
-    <>
-      <h1>TODO: Finance Analyst Dashboard and Report List</h1>
-      <h3>
-        <Link to="/review-reports"> Reports to review list</Link>
-      </h3>
-    </>
+    <Grid container spacing={2} md={12} justify="center">
+      <Grid item>
+      <Typography variant="h3">Finance Analyst Dashboard</Typography>
+      </Grid>
+      <Grid item md={8}>
+        <Box p={4} boxShadow={2} borderRadius={4} justifyContent="space-between" display="flex">
+          <Grid item>
+            <Typography variant="h4">
+              Quarterly Report States
+            </Typography>
+            <Typography variant="subtitle1">
+              Reports In Progress: {reportsInProgress}
+            </Typography>
+            <Typography variant="subtitle1">
+              Reports Submitted to DIO: {reportsSubmitted}
+            </Typography>
+            <Typography variant="subtitle1">
+              Reports That Require Follow Up: {reportsToFollowup}
+            </Typography>
+            <Typography variant="subtitle1">
+              Reports Approved: {reportsApproved}
+            </Typography>
+          </Grid>
+          <Grid item md={4} container direction="column-reverse">
+            <Button
+              color="primary"
+              variant="outlined"
+              type="button"
+              style={{margin: '4px'}}
+              onClick={()=> alert('TODO: implement quarterly report summary view for current reporting period')}
+            >
+              Q2 FY 20/21 Summary
+            </Button>
+            <Button
+              color="primary"
+              variant="outlined"
+              type="button"
+              style={{margin: '4px'}}
+              onClick={()=> history.push("/review-reports")}
+            >
+              Review Reports
+            </Button>
+          </Grid>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
